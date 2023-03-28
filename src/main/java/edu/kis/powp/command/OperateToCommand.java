5b59288871813ca43fa7d05cb69d351a34b56f3d
DMisztal <dominikmisztal@onet.eu>
2023-03-28 12:35:26 +0200

    implemented operateTo

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,5 +1,14 @@
 package edu.kis.powp.command;
 
-public class OperateToCommand {
+import edu.kis.powp.jobs2d.drivers.adapter.Jobs2DToDrawerAdapter;
+
+public class OperateToCommand implements CommandInterface {
+    private Jobs2DToDrawerAdapter adapter;
+    private int x;
+    private int y;
     
+    @Override
+    public void execute(){
+        adapter.operateTo(x, y);
+    }
 }
